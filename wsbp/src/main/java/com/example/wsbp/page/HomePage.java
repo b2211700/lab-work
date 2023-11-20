e04341b3bde3da4a247710bc5af3554b17d7b667
b2211700 <b2211700@photon.chitose.ac.jp>
2023-11-20 14:46:02 +0900

    動作確認

---
+++ wsbp/src/main/java/com/example/wsbp/page/HomePage.java
@@ -0,0 +1,51 @@
+package com.example.wsbp.page;
+
+import com.giffing.wicket.spring.boot.context.scan.WicketHomePage;
+import org.apache.wicket.markup.html.WebPage;
+import org.springframework.boot.Banner;
+import org.wicketstuff.annotation.mount.MountPath;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.model.Model;
+import com.example.wsbp.service.ISampleService;
+import org.apache.wicket.spring.injection.annot.SpringBean;
+import org.apache.wicket.markup.html.link.BookmarkablePageLink;
+
+@WicketHomePage
+@MountPath("Home")
+public class HomePage extends WebPage {
+
+    // 下の2行を追加
+    @SpringBean
+    private ISampleService service;
+
+    public HomePage() {
+        // "Wicket-Spring-Boot" を表示するLabel
+        var youModel = Model.of("Wicket-Spring-Boot");
+        var youLabel = new Label("you", youModel);
+        add(youLabel);
+
+        // 学籍番号を表示するLabel
+        var gakusekiModel = Model.of("b2211700");
+        var gakusekiLabel = new Label("gakuseki", gakusekiModel);
+        add(gakusekiLabel);
+
+        // 名前を表示するlabel
+        var nameModel = Model.of("鳥谷部花心");
+        var nameLabel = new Label("name", nameModel);
+        add(nameLabel);
+
+        // 現在の時刻を表示するlabel
+        var timeModel = Model.of(service.makeCurrentHMS());
+        var timeLabel = new Label("time", timeModel);
+        add(timeLabel);
+
+        // 0~9の乱数を表示するlabel
+        var randModel = Model.of(service.makeRandInt());
+        var randLabel = new Label("rand", randModel);
+        add(randLabel);
+
+        var toUserMakerLink = new BookmarkablePageLink<>("toUserMaker", UserMakerPage.class);
+        add(toUserMakerLink);
+    }
+
+}
